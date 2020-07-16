9260b1423c0804ddb03f9e1e8468de628baa1ca7
asadsanto10 <santo.meridian@gmail.com>
2020-07-16 14:47:49 +0600

    m

--- jsProblem/removeDuplicateArray.js
+++ jsProblem/removeDuplicateArray.js
@@ -1,6 +1,10 @@
 var number = [10, 45, 41, 78, 10, 60, 77, 45, 48, 10]
 var uniqueNUmber = [];
 for( var i = 0; i < number.length; i++){
-    var elemnt = name[i];
+    var element = number[i];
     var index = uniqueNUmber.indexOf(element);
+    if(index == -1){
+        uniqueNUmber.push(element);
+    }
 }
+console.log(uniqueNUmber);
